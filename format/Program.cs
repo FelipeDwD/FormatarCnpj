e31b14ec5c5f3a6699867e97a7d1e5ce40f260eb
Felipe Neves de Arruda <50635026+FelipeDwD@users.noreply.github.com>
2020-02-21 11:48:20 -0300

    2020-02-21

---
+++ format/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cnpjNumerico = "08575666000180";
+            Console.WriteLine("Cnpj somente números: " + cnpjNumerico);          
+            string cnpj = cnpjNumerico.Substring(0, 2) + ".";
+            cnpj += cnpjNumerico.Substring(2, 3) + ".";
+            cnpj += cnpjNumerico.Substring(5, 3) + "/";
+            cnpj += cnpjNumerico.Substring(8, 4) + "-";
+            cnpj += cnpjNumerico.Substring(12, 2);
+
+            cnpjNumerico = cnpj;
+            Console.WriteLine("\n\n\nCnpj: " + cnpjNumerico);
+        }
+    }
+}
